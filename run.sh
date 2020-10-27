f8ed820cb0d634f27381a0aa1d4923ca1de77181
Mihail Golban <mihail.g@goparrot.ai>
2020-10-27 22:22:57 +0200

    update bitcoin.conf

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #
 # Run a container with port 18332 (regtest) exposed
 #
-docker run -it --net=bridge --name=bitcoin-alpine -p 18332:18332  bitcoin-alpine
+docker run -it --net=bridge --name=bitcoin-alpine -p 18443:18443 bitcoin-alpine
